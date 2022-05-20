afab2b294a505d0101e7f3ed2ed3928bbd8b59f6
Michel Amorim <michelmsa7799@gmail.com>
2022-05-20 19:18:00 -0300

    feat: init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
 
-function App() {
+import "./App.css";
+
+const App: React.FC = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const handleSubmit = useCallback(
+    (event: any) => {
+      console.log(event);
+    },
+    [name, age]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Formulario</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="name"
+          type="text"
+          placeholder="Name..."
+          className="form-control mb2"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          name="age"
+          type="number"
+          placeholder="Age..."
+          className="form-control mb2"
+          onChange={(e) => setAge(e.target.value)}
+        />
+
+        <button className="btn btn-primary btn-block" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
